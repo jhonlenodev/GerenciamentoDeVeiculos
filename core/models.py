54c09285642f4b28fab8bda469d5e02d8f520c84
Mask <planosmask@gmail.com>
2021-06-28 15:33:33 -0300

    Adicionando Templates

--- core/models.py
+++ core/models.py
@@ -3,6 +3,15 @@ from django.db import models
 class Veiculo(models.Model):
     modelo = models.CharField(max_length=200)
     marca = models.CharField(max_length=100)
-    
+    ano = models.IntegerField()
+    placa = models.CharField(max_length=10 , unique=True)
+    cor = models.CharField(max_length=100)
+    chassi = models.CharField(max_length=50, unique=True)
     data_compra = models.DateField(auto_now_add=True)
+    valor = models.FloatField()
     
+class Venda(models.Model):
+    valor_venda = models.FloatField()
+    data_compra = models.DateField(auto_now_add=True)
+    comisao = models.FloatField()
+    lucro = models.FloatField()
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from core.models import *
 
-# Create your views here.
+def home(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'home.html', {'veiculos':veiculos})
